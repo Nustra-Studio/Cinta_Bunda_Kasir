2916d95cb9fd669cc601ac910284e92aec55be5e
Novan Fitra <novanfitra23@gmail.com>
2023-08-28 11:37:00 +0700

    Settig diskon

--- GUI/Supervisor/Setting.cs
+++ GUI/Supervisor/Setting.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using KasirApp.View;
+using KasirApp.Repository;
+using KasirApp.Model;
 
 namespace KasirApp.GUI.Supervisor
 {
-    public partial class Setting : Form
+    public partial class Setting : Form, iSetting
     {
+        
+
         public Setting()
         {
             InitializeComponent();
+            var _repo = new SettingRepo(this);
+            _repo.Set();
+        }
+
+        public void SetValue(SettingModel model)
+        {
+            txtKaryawan.Text = model.Karyawan;
+            txtReseller.Text = model.Reseller;
+            AutoDiskon.Text = model.Autodiskon;
+        }
+
+        public void Numeric(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            var model = new SettingModel();
+            model.Karyawan = txtKaryawan.Text;
+            model.Reseller = txtReseller.Text;
+            model.Autodiskon = AutoDiskon.Text;
+
+            var _repo = new SettingRepo(this);
+            _repo.UpdateSetting(model);
+            
         }
     }
 }
---
+++ Repository/SettingRepo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using KasirApp.Model;
+using KasirApp.View;
+
+namespace KasirApp.Repository
+{
+    public class SettingRepo
+    {
+        Operator op = new Operator();
+        MboxOperator mb = new MboxOperator();
+        iSetting _set;
+        public SettingRepo(iSetting set)
+        {
+            _set = set;
+        }
+
+        public void Set()
+        {
+            using (var cmd = new MySqlCommand("select * from settings", op.Conn))
+            {
+                op.KonekDB();
+                using (var rd = cmd.ExecuteReader())
+                {
+                    rd.Read();
+                    if (rd.HasRows)
+                    {
+                        var model = new SettingModel();
+                        model.Karyawan = rd["presentase_karyawan"].ToString();
+                        model.Reseller = rd["presentase_reseller"].ToString();
+                        model.Autodiskon = rd["auto_diskon"].ToString();
+                        _set.SetValue(model);
+                    }
+                }
+            }
+        }
+
+        public void UpdateSetting(SettingModel model)
+        {
+            if (model.Karyawan == "" || model.Reseller == "" || model.Autodiskon == "")
+            {
+                mb.PeringatanOK("Lengkapi Data");
+            }
+            using (var cmd = new MySqlCommand("UPDATE settings SET presentase_karyawan = @karyawan,presentase_reseller = @reseller, auto_diskon = @diskon where id = 1", op.Conn))
+            {
+                cmd.Parameters.AddWithValue("@karyawan", model.Karyawan);
+                cmd.Parameters.AddWithValue("@reseller", model.Reseller);
+                cmd.Parameters.AddWithValue("@diskon", model.Autodiskon);
+
+                op.KonekDB();
+                cmd.ExecuteNonQuery();
+                op.KonekDB();
+
+                mb.InformasiOK("Berhasil Di Simpan");
+                Set();
+            }
+        }
+    }
+}
